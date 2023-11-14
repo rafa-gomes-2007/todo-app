bed6c4359f70045264204daa15b83727c2afe636
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-11-13 21:54:03 -0300

    primeira execucao do servidor

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (requisicao,resposta) => {
+    resposta.send('Ola mundo')
+})
+
+app.listen(3000, () => {
+    console.log("servidor rodando na porta 3000")
+})
+
