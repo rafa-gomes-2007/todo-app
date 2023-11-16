2e45be6d917e2ec8332be8f837bed9491b9c92f4
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-11-15 21:01:40 -0300

    primeira view

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
-const exphbs = requiere("express-handlebars")
+const exphbs = require("express-handlebars")
 
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
-app.get('view engine','handlebars')
+app.set('view engine', 'handlebars')
 
-app.get('/', (requisicao,resposta) => {
-    resposta.send('Ola mundo')
+app.get('/', (requisicao, resposta) => {
+    resposta.render('main')
 })
 
 app.listen(3000, () => {
