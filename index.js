5cfeeeb501f448e08bd3ff69cf5ccde54eada09e
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-11-26 20:11:46 -0300

    adição das imagens

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express")
-const exphbs = require("express-handlebars")
+const hbs = require("express-handlebars")
 
 const app = express()
 
-app.engine('handlebars', exphbs.engine())
+app.engine('handlebars', hbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
-    resposta.render('main')
+    resposta.render('home')
 })
 
-app.listen(3000, () => {
+app.listen(3000, () => {    
     console.log("servidor rodando na porta 3000")
 })
-
