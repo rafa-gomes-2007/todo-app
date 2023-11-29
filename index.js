0754942112cc82dc52535ebb8e63ade46742c1ea
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-11-28 21:18:18 -0300

    iniciando leitura dos dados do banco de dados

--- index.js
+++ index.js
@@ -37,6 +37,28 @@ app.post('/criar', (requisicao, resposta) => {
 
 
 app.get('/', (requisicao, resposta) => {
+    const sql = 'SELECT * FROM tarefas'
+
+
+    conexao.query(sql, (erro, dados) => {
+
+        if (erro) {
+            return console.log(erro)
+        }
+
+        
+
+        const tarefas = dados.map((dados) => {
+            return {
+                id: dados.id,
+                descricao: dados.descricao,
+                completa: dados.completa === 0  ? false : true
+            }
+        })
+
+      
+    })
+
     resposta.render('home')
 })
 
