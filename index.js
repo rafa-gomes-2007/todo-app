b2a70c10363639b302a6d1238301af499f480af6
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-11-28 21:01:53 -0300

    salvando tarefa no banco de dados

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+//converter daados do formulario para javascript
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas (descricao, completa)
+        Values('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+           return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
+
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
@@ -18,6 +45,7 @@ const conexao = mysql.createConnection({
     user: "root",
     password: "root",
     database: "todo-app",
+    port: "3306"
 })
 
 
